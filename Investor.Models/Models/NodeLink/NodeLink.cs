78e14652b8f7ad91b2e4d213d4f8be9489ab883c
carsch <carl@meridium.se>
2016-04-14 14:13:08 +0200

    adding nodelink

---
+++ Investor.Models/Models/NodeLink/NodeLink.cs
@@ -0,0 +1,21 @@
+﻿using Investor.Models.Models.NodeLink.Abstraction;
+
+namespace Investor.Models.Models.NodeLink
+{
+    public class NodeLink : INodeLink
+    {
+        public string Caption { get; set; }
+
+        public string Link { get; set; }
+
+        public bool NewWindow { get; set; }
+
+        public bool IsInternal { get; set; }
+
+        public string Internal { get; set; }
+
+        public string Type { get; set; }
+
+        public string Title { get; set; }
+    }
+}

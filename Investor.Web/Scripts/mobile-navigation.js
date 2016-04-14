d92742111fc96f6e2656521be71531c26b1a4340
Daniel Bäckelin <daniel@meridium.se>
2016-04-14 16:02:36 +0200

    continue mobile-menu

--- Investor.Web/Scripts/mobile-navigation.js
+++ Investor.Web/Scripts/mobile-navigation.js
@@ -1,6 +1,13 @@
 ﻿$(document).ready(function() {
   $("#mobile-main-menu .ui").click(function (e) {
     e.preventDefault();
+
+    if ($("#mobile-main-menu .options").is(":visible")) {
+      $("#mobile-main-menu").removeClass("selected");
+    } else {
+      $("#mobile-main-menu").addClass("selected");
+    }
+
     $("#mobile-main-menu .options").slideToggle();
   });
 });

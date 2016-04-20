265ef102651f9ce0f26965ea37f2472f733ac583
carsch <carl@meridium.se>
2016-04-20 11:33:58 +0200

    adding iframe

---
+++ Investor.Models/PageModels/IFramePageModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using UCodeFirst;
+using UCodeFirst.ContentTypes;
+using UCodeFirst.Tab;
+using Umbraco.Core.Models;
+
+namespace Investor.Models.PageModels
+{
+    [ContentType(
+        DisplayName = "Sida: Iframe",
+        Icon = Icon.WindowPopin,
+        Description = "En sida för att skapa en iframe.",
+        AllowAtRoot = true
+    )]
+    public class IFramePageModel : BaseModel
+    {
+        #region constructors
+
+        public IFramePageModel(IPublishedContent content, CultureInfo culture) : base(content, culture)
+        {
+        }
+
+        public IFramePageModel(IPublishedContent content) : base(content)
+        {
+        }
+
+        #endregion
+
+        #region content
+        
+        [Property(
+            UmbracoDataType.Textstring,
+            Tab.Content,
+            DisplayName = "Länk till iframe",
+            Description = ""
+        )]
+        public virtual string IFrameSrc { get; set; }
+        
+        #endregion
+    }
+}

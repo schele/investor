2af6981900cde1782025129041bc5ff089706f84
Daniel Bäckelin <daniel@meridium.se>
2016-04-21 15:22:12 +0200

    Begin campaign-popup

---
+++ Investor.Web/Scripts/campaign-lightbox.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+
+  $('#fancy_box').magnificPopup({
+    type: 'inline',
+    closeBtnInside: false,
+    closeBtnOutside: false
+    // other options
+  }).trigger("click");;
+});

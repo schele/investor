3a773e80851441913323cbe28623258a3605cc72
Daniel Bäckelin <daniel@meridium.se>
2016-04-22 14:10:12 +0200

    Set 24hour cookie for campaign-popup

--- Investor.Web/Scripts/campaign-lightbox.js
+++ Investor.Web/Scripts/campaign-lightbox.js
@@ -1,14 +1,45 @@
 ﻿$(document).ready(function () {
 
-  $('#fancy_box').magnificPopup({
-    type: 'inline',
-    closeBtnInside: false,
-    closeBtnOutside: false
-    // other options
-  }).trigger("click");;
+  if (!readCookie('hide-campaign-popup')) {
+    $('#fancy_box').magnificPopup({
+      type: 'inline',
+      closeBtnInside: false,
+
+    }).trigger("click");;
+  }
+
+  $.magnificPopup.instance.close = function () {
+    createCookie('hide-campaign-popup', true, 1);
+    $.magnificPopup.proto.close.call(this);
+  };
 
   $('#campaign-custom-close, #campaign-custom-close-mobile').click(function () {
     $.magnificPopup.close();
   });
 
 });
+
+function createCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    var expires = "; expires=" + date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name, "", -1);
+}

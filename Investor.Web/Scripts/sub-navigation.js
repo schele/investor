c2e2689e3cfa06dc7f59355d6cab581ae550a244
Daniel Bäckelin <daniel@meridium.se>
2016-04-23 15:03:58 +0200

    Add sub-navigation styles

---
+++ Investor.Web/Scripts/sub-navigation.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('.has-children').removeClass('.has-children--expanded');
+
+    if ($('.sub-navigation-active').parents('.has-children').length > 0) {
+      $('.sub-navigation-active').parents('.has-children').addClass('has-children--expanded');
+    }
+
+    if ($('.sub-navigation-active').children('ul').length > 0) {
+      $('.sub-navigation-active').addClass('has-children--expanded');
+    }
+});

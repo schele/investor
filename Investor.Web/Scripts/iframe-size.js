371f2366cf24cfd9c6737a9cb26c59549dd02b2d
Daniel Bäckelin <daniel@meridium.se>
2016-04-24 15:21:33 +0200

    Add iframe-page styles and sub-navigation styles

---
+++ Investor.Web/Scripts/iframe-size.js
@@ -0,0 +1,8 @@
+﻿function iframeLoaded() {
+  var iFrameId = document.getElementById('iframe-page-iframe');
+  if (iFrameId) {
+    // here you can make the height, I delete it first, then I make it again
+    iFrameId.height = "";
+    iFrameId.height = iFrameId.contentWindow.document.body.scrollHeight + "px";
+  }
+}
--- Investor.Web/Scripts/sub-navigation.js
+++ Investor.Web/Scripts/sub-navigation.js
@@ -8,4 +8,16 @@
     if ($('.sub-navigation-active').children('ul').length > 0) {
       $('.sub-navigation-active').addClass('has-children--expanded');
     }
+
+    $(".sub-navigation-mobile-btn").click(function (e) {
+      e.preventDefault();
+
+      if ($(".sub-navigation-nav").is(":visible")) {
+        $(".sub-navigation-nav").removeClass("selected");
+      } else {
+        $(".sub-navigation-nav").addClass("selected");
+      }
+
+      $(".sub-navigation-nav").stop().slideToggle();
+    });
 });

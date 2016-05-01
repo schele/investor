7505355e37f4817379fa7e6e4049037f3f8cbd70
Daniel Bäckelin <daniel@meridium.se>
2016-05-01 23:17:12 +0200

    Add new cookie-script and styles

---
+++ Investor.Web/Scripts/cookies.js
@@ -0,0 +1,26 @@
+﻿var moduleContainer = $(".cookie-information");
+var _this;
+
+var events = {
+  _init: function () {
+    var cookieValue = '';
+    _this = this;
+
+    if (document.cookie) {
+      cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)acceptCookie\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+    }
+
+    if (cookieValue !== '1') {
+      moduleContainer.addClass('cookie-information--visible');
+    }
+  },
+  _onclick: $('.cookie-information__inner__button').click(function (e) {
+    e.preventDefault();
+    var acceptDate = new Date(2020, 1, 1);
+    document.cookie = 'acceptCookie=1; path=/; expires=' + acceptDate.toUTCString();
+    moduleContainer.removeClass('cookie-information--visible');
+  })
+}
+
+events._init();
+events._onclick();

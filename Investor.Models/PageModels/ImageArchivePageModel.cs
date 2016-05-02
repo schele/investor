b746586fd8ceb6829ea332b9b5fd4d38d9b577eb
carsch <carl@meridium.se>
2016-05-02 10:02:09 +0200

    adding image gallery props

--- Investor.Models/PageModels/ImageArchivePageModel.cs
+++ Investor.Models/PageModels/ImageArchivePageModel.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Investor.Models.Converters;
+using Investor.Models.Models.NodeLink;
+using Investor.Models.PageModels.Interfaces;
 using UCodeFirst;
 using UCodeFirst.ContentTypes;
+using UCodeFirst.Tab;
 using Umbraco.Core.Models;
 
 namespace Investor.Models.PageModels
 {
     [ContentType(
     DisplayName = "Sida: Bildarkiv",
-    Icon = Icon.Users,
+    Icon = Icon.PhotoAlbum,
     Description = "En sida för att skapa ett bildarkiv",
     AllowAtRoot = false,
     AllowedChildNodes = new object[]
@@ -20,8 +20,10 @@ namespace Investor.Models.PageModels
             typeof(ImageArchivePageModel)
         }
     )]
-    public class ImageArchivePageModel : BaseModel
+    public class ImageArchivePageModel : BaseModel, IPush
     {
+        #region constructors
+
         public ImageArchivePageModel(IPublishedContent content, CultureInfo culture) : base(content, culture)
         {
         }
@@ -29,5 +31,45 @@ namespace Investor.Models.PageModels
         public ImageArchivePageModel(IPublishedContent content) : base(content)
         {
         }
+
+        #endregion
+
+        #region puff
+
+        [Property(
+            UmbracoDataType.MediaPicker,
+            Tab.Push,
+            DisplayName = "Puff: Bild",
+            Description = "Denna bild visas på en puffyta",
+            Converter = typeof(PublishedMediaConverter)
+        )]
+        public virtual IPublishedContent PushImage { get; set; }
+
+        [Property(
+            UmbracoDataType.TextboxMultiple,
+            Tab.Push,
+            DisplayName = "Puff: Text",
+            Description = "Text som visas på en puffyta"
+        )]
+        public virtual string PushText { get; set; }
+
+        [Property(
+            UmbracoDataType.Textstring,
+            Tab.Push,
+            DisplayName = "Puff Relaterade länkar: Rubrik",
+            Description = "Rubrik för relaterade länkar"
+        )]
+        public virtual string RelatedLinksForPushHeader { get; set; }
+
+        [Property(
+            UmbracoDataType.RelatedLinks,
+            Tab.Push,
+            DisplayName = "Puff Relaterade länkar: Länkar",
+            Description = "Dessa länkar visas på en puffyta",
+            Converter = typeof(NodeLinkConverter<NodeLink>)
+        )]
+        public virtual IEnumerable<NodeLink> RelatedLinksForPush { get; set; }
+
+        #endregion
     }
 }

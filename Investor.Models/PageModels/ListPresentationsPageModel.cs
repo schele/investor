2d60aa0e2f296c2cc62d0a96bcb13b964f1fdab8
carsch <carl@meridium.se>
2016-05-06 07:57:16 +0200

    adding list presentations page model

---
+++ Investor.Models/PageModels/ListPresentationsPageModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Investor.Models.Converters;
+using Investor.Models.Models.NodeLink;
+using UCodeFirst;
+using UCodeFirst.ContentTypes;
+using UCodeFirst.Tab;
+using Umbraco.Core.Models;
+
+namespace Investor.Models.PageModels
+{
+    [ContentType(
+        DisplayName = "Sida: ListPresentationPageModel",
+        Icon = Icon.Presentation,
+        Description = "En sida för att lista ListPresentationPageModel",
+        AllowAtRoot = false
+    )]
+    public class ListPresentationPageModel : BaseModel
+    {
+        #region constructors
+
+        public ListPresentationPageModel(IPublishedContent content, CultureInfo culture) : base(content, culture)
+        {
+        }
+
+        public ListPresentationPageModel(IPublishedContent content)
+            : base(content)
+        {
+        }
+
+        #endregion
+
+        #region content
+     
+        [Property(
+            UmbracoDataType.Textstring,
+            Tab.Content,
+            DisplayName = "Rubrik",
+            Description = "H1"
+        )]
+        public virtual string Header { get; set; }
+
+        [Property(
+            UmbracoDataType.TextboxMultiple,
+            Tab.Content,
+            DisplayName = "Ingress",
+            Description = ""
+        )]
+        public virtual string Preamble { get; set; }
+
+        [Property(
+            UmbracoDataType.Textstring,
+            Tab.Content,
+            DisplayName = "Länkar: Rubrik",
+            Description = ""
+        )]
+        public virtual string RelatedLinksHeader { get; set; }
+
+        [Property(
+            UmbracoDataType.RelatedLinksWithMedia,
+            Tab.Content,
+            DisplayName = "Länkar: Länkar",
+            Description = "",
+            Converter = typeof(NodeLinkConverter<NodeLink>)
+        )]
+        public virtual IEnumerable<NodeLink> RelatedLinks { get; set; }
+        
+        #endregion        
+    }
+}

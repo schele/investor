ae8592da0d458e495617816b2409c82bf1ded7c5
carsch <carl@meridium.se>
2016-05-09 13:52:57 +0200

    adding investor and media for start page

---
+++ Investor.Web/Configuration/SiteConstants.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace Investor.Configuration
+{
+    public static class SiteConstants
+    {
+        public static string LatestAnnualReport
+        {
+            get { return ConfigurationManager.AppSettings["latest-annual-report"]; }
+        }
+
+        public static string LatestInterimReport
+        {
+            get { return ConfigurationManager.AppSettings["latest-interim-report"]; }
+        }
+
+        public static string CustomQuote
+        {
+            get { return ConfigurationManager.AppSettings["custom-quote"]; }
+        }
+    }
+}

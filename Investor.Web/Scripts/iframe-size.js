858b53a2dd512b9a74ae66be32ba70251e27f3ad
Daniel Bäckelin <daniel@meridium.se>
2016-05-11 14:57:31 +0200

    Add resize on iframe

--- Investor.Web/Scripts/iframe-size.js
+++ Investor.Web/Scripts/iframe-size.js
@@ -1,8 +1,1 @@
-﻿function iframeLoaded() {
-  var iFrameId = document.getElementById('iframe-page-iframe');
-  if (iFrameId) {
-    // here you can make the height, I delete it first, then I make it again
-    iFrameId.height = "";
-    iFrameId.height = iFrameId.contentWindow.document.body.scrollHeight + "px";
-  }
-}
+﻿$('iframe').iFrameResize([{}]);

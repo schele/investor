9e94969d32bfdebf482ad3a28d415923249fff0c
carsch <carl@meridium.se>
2016-05-15 20:02:21 +0200

    adding css for startpage modules

---
+++ Investor.Models/Extensions/WebViewPageExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Investor.Models.Extensions
+{
+    public static class WebViewPageExtensions
+    {
+        public static bool IsEdit(this WebViewPage viewPage)
+        {
+            return viewPage.Request.Url != null && viewPage.Request.Url.AbsolutePath.Contains("GetMacroResultAsHtmlForEditor");
+        }
+    }
+}
